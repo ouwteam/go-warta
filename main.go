af9b825dc4c779852aad0edee7ed3cbec4c9af81
Asep Oo Kurniawan <kurniawan9411n@gmail.com>
2021-05-04 04:36:54 +0700

    change address to env

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 
 	srv := &http.Server{
 		Handler: r,
-		Addr:    "127.0.0.1:8000",
+		Addr:    os.Getenv("SERVER_ADDR") + ":" + os.Getenv("SERVER_PORT"),
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
